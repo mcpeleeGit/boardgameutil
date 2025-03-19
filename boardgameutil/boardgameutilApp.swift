a757f1212ee4c27f0b425f20eb6f46aeffad48b7
mcpeleeGit <mcpelee@gmail.com>
2025-03-19 20:19:48 +0900

    Initial Commit

---
+++ boardgameutil/boardgameutilApp.swift
@@ -0,0 +1,17 @@
+//
+//  boardgameutilApp.swift
+//  boardgameutil
+//
+//  Created by kakao on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct boardgameutilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ boardgameutilTests/boardgameutilTests.swift
@@ -0,0 +1,17 @@
+//
+//  boardgameutilTests.swift
+//  boardgameutilTests
+//
+//  Created by kakao on 3/19/25.
+//
+
+import Testing
+@testable import boardgameutil
+
+struct boardgameutilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
